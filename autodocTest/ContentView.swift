17425cb6316236faecfe4541e11ec14f45fda34d
Dmitry Martynov <mdymailru@gmail.com>
2022-06-24 22:30:44 +0300

    Initial Commit

---
+++ autodocTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  autodocTest
+//
+//  Created by Dmitry Martynov on 24.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ autodocTest/autodocTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  autodocTestApp.swift
+//  autodocTest
+//
+//  Created by Dmitry Martynov on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct autodocTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
